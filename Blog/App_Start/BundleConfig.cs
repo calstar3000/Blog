8d27225d24587cc2cda96dbda2ef946d84116704
Calum O'Neil <calum.oneil@gmail.com>
2016-03-05 17:23:03 +1300

    Add simple jQuery plugin to render posts on the homepage by talking to the API

--- Blog/App_Start/BundleConfig.cs
+++ Blog/App_Start/BundleConfig.cs
@@ -20,6 +20,9 @@ namespace Blog
 					  "~/Scripts/bootstrap.js",
 					  "~/Scripts/respond.js"));
 
+			bundles.Add(new StyleBundle("~/bundles/js").Include(
+					  "~/Scripts/home.js"));
+
 			bundles.Add(new StyleBundle("~/Content/css").Include(
 					  "~/Content/bootstrap.css",
 					  "~/Content/site.css"));
---
+++ Blog/Scripts/home.js
@@ -0,0 +1,21 @@
+﻿$.fn.LoadPosts = function () {
+	var $this = $(this);
+	$.getJSON("/api/blog/posts/", null, function (data) {
+		console.log(data);
+
+		$.each(data, function () {
+			var post = this;
+			var $comments = $("<ul></ul>");
+			
+			$.each(post.comments, function () {
+				$comments.append("<li><p>" + this.body + "</p></li>")
+			});
+			
+			$this.append("<li><p>" + this.datePublished + " - " + this.title + "</p><p>" + this.body + "</p>" + $comments[0].outerHTML + "</li>");
+		});
+	});
+};
+
+$(document).ready(function () {
+	$("#postList").LoadPosts();
+});

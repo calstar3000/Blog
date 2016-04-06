42a4bd76443a1c488ed5d637991cad974ccfc743
Calum O'Neil <calum.oneil@gmail.com>
2016-04-06 22:48:33 +1200

    Move web stuff from API to web project

--- Blog.Web/Controllers/HomeController.cs
+++ Blog.Web/Controllers/HomeController.cs
@@ -1,27 +1,21 @@
 ﻿using System.Web.Mvc;
 
-namespace Blog.API.Controllers
+namespace Blog.Web.Controllers
 {
 	public class HomeController : Controller
 	{
 		public ActionResult Index()
 		{
-			ViewBag.Title = "There will be bugs";
-
 			return View();
 		}
 
 		public ActionResult New()
 		{
-			ViewBag.Title = "There will be bugs";
-
 			return View();
 		}
 
 		public ActionResult Register()
 		{
-			ViewBag.Title = "There will be bugs";
-
 			return View();
 		}
 	}

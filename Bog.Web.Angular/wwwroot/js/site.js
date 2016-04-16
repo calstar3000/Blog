f2018431d7adbcb17ab516ea4224ca11a3165193
Calum O'Neil <calum.oneil@gmail.com>
2016-04-16 14:19:12 +1200

    Add ASP.NET 5 project for angular and some setup code

---
+++ Bog.Web.Angular/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿// Write your Javascript code.
+
+angular.module('blogApp', ['ui.bootstrap'])
+	.run(function () {
+		console.log('Hello, world, from blogApp!');
+	})
+	.directive('currentTime', function () {
+		return {
+			template: '<h2 class="text-center">The time is {{vm.currentTime}}</div>',
+			controllerAs: 'vm',
+			controller: function () {
+				var vm = this;
+				vm.currentTime = new Date().toLocaleTimeString();
+			}
+		}
+	});
